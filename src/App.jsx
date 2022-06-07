b5b217412c314da0d0e31672971e3018285313c3
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-06-06 21:55:20 -0700

    renders about, works. and contact components in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.scss';
+import About from './components/about/about';
+import Works from './components/works/works';
+import Contact from './components/contact/contact';
 
 function App() {
   return (
     <div className="App">
+      <About/>
+      <Works/>
+      <Contact/>
     </div>
   );
 }
